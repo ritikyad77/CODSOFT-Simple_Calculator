72f7c89f29613ac72b15dab508ad471c1fafb9ac
ritikyad77 <ritikyadav3005@gmail.com>
2024-06-06 13:50:40 +0530

    Add files via upload

---
+++ Simple_Calculator.py
@@ -0,0 +1,48 @@
+
+
+# Function to add two numbers
+def add(num1, num2):
+	return num1 + num2
+
+# Function to subtract two numbers
+def sub(num1, num2):
+	return num1 - num2
+
+# Function to multiply two numbers
+def mul(num1, num2):
+	return num1 * num2
+
+# Function to divide two numbers
+def div(num1, num2):
+	return num1 / num2
+
+print("Please select operation -\n" \
+		"1. Add\n" \
+		"2. Subtract\n" \
+		"3. Multiply\n" \
+		"4. Divide\n")
+
+
+# Take input from the user
+select = int(input("Select operations form 1, 2, 3, 4 :"))
+
+number_1 = int(input("Enter first number: "))
+number_2 = int(input("Enter second number: "))
+
+if select == 1:
+	print(number_1, "+", number_2, "=",
+					add(number_1, number_2))
+
+elif select == 2:
+	print(number_1, "-", number_2, "=",
+					sub(number_1, number_2))
+
+elif select == 3:
+	print(number_1, "*", number_2, "=",
+					mul(number_1, number_2))
+
+elif select == 4:
+	print(number_1, "/", number_2, "=",
+					div(number_1, number_2))
+else:
+	print("Invalid input")
